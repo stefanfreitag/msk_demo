beb91995207437886ee2d9e1c6be381e46d39759
Stefan Freitag <stefan.freitag@udo.edu>
2019-08-18 22:11:05 +0200

    Added stack environment information
    Added creation of an EC2 instance
    Reworked subnet setup

--- bin/msk_demo.ts
+++ bin/msk_demo.ts
@@ -4,4 +4,9 @@ import cdk = require('@aws-cdk/core');
 import { MskDemoStack } from '../lib/msk_demo-stack';
 
 const app = new cdk.App();
-new MskDemoStack(app, 'MskDemoStack');
+new MskDemoStack(app, 'MskDemoStack', {
+    env: {
+        region: 'eu-west-1',
+        account: 'ACCOUNTID'
+    }
+});
